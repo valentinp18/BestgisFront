80b2c7f9ce750206b3db2a1175a01d0f8a95be0f
valentinp18 <i2219990@continental.edu.pe>
2024-07-07 18:02:45 -0500

    Descripción del commit

--- src/app/app.confing.ts
+++ src/app/app.confing.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './services/auth.interceptor';
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
@@ -11,22 +11,16 @@ import { provideAuth } from '@angular/fire/auth';
 import { provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-  ],
-  providers: [
-    AuthInterceptor,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => provideAuth()), // Ajusta esto según la documentación de AngularFire
-    provideFirestore(() => provideFirestore()), // Ajusta esto según la documentación de AngularFire
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [
+        AuthInterceptor,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => provideAuth()), // Ajusta esto según la documentación de AngularFire
+        provideFirestore(() => provideFirestore()),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppConfig { }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ModalModule.forRoot(),
-    HttpClientModule, // ==> sin este no podemos consumir ningun servio web
-    PaginationModule.forRoot()
-  ], 
-  exports:[
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ModalModule,
-    PaginationModule
-
-  ]
-})
+@NgModule({ declarations: [],
+    exports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ModalModule,
+        PaginationModule
+    ], imports: [CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ModalModule.forRoot(), // ==> sin este no podemos consumir ningun servio web
+        PaginationModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule { }
